23189fd53c3d5a846616cdd1918ba12c2ae6c4e0
Mateus Moraes <mtu_h@hotmail.com>
2025-02-25 23:42:14 -0300

    Angular SQLite teste

--- Frontend/AngularGUI/src/app/app.component.ts
+++ Frontend/AngularGUI/src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { sqlite3 as SQLite } from 'sqlite3';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'AngularGUI';
+
+  constructor(private sqlite: SQLite) {
+    sqlite = new SQLite
+  }
+
+  loadDB() {}
 }
