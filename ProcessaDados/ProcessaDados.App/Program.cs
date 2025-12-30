6ec24282d7b80e3fcfb7aa0b4c7988264d4862e9
Hardrew Desu <mtu_h@hotmail.com>
2025-12-30 17:12:57 -0300

    v.1.0.5

--- ProcessaDados/ProcessaDados.App/Program.cs
+++ ProcessaDados/ProcessaDados.App/Program.cs
@@ -14,7 +14,7 @@ Log.Logger = new LoggerConfiguration()
     .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}")
     .CreateLogger();
 
-Log.Information("Aplicação iniciada: v1.0.4");
+Log.Information("Aplicação iniciada: v1.0.5");
 
 const string filePath = "config.json";
 
